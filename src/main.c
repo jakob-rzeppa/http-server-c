40ce3e229336cf5022ef9202e10321bc40b1f3ab
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-19 20:47:04 +0000

    created a basic http server, that returns a json response success

--- src/main.c
+++ src/main.c
@@ -1,7 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-int main()
+#define PORT 8080
+#define MAX_QUEUED_CONNECTIONS 3
+
+void listenForConnections(int server_fd)
 {
-    printf("Welcome to Docker!!!\n");
-    return 0;
+    printf("Listening to connections...\n");
+
+    while (1)
+    {
+        // client info
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_socket;
+
+        client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0)
+        {
+            perror("accept failed");
+            continue;
+        }
+
+        printf("connection recieved");
+
+        const char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: 27\r\n"
+            "\r\n"
+            "{ \"status\": \"success\" }";
+
+        send(client_socket, response, strlen(response), 0);
+
+        close(client_socket);
+        client_socket = -1;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_socket;
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = PF_INET;         // uses the internet namespace
+    server_addr.sin_addr.s_addr = INADDR_ANY; // the address to accept incoming messages
+    server_addr.sin_port = htons(PORT);       // htons converts a 16-bit int from host byte order to network byte order
+
+    // creating the socket - 0 for default protocol (for SOCK_STREAM TCP)
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket connected\n");
+
+    // binding the socket to the PORT
+    if ((bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0)
+    {
+        perror("bind failed");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket bound to PORT %d\n", PORT);
+
+    // listening to the socket
+    if ((listen(server_socket, MAX_QUEUED_CONNECTIONS)) < 0)
+    {
+        perror("listen failed");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    listenForConnections(server_socket);
+
+    printf("Shutdown server");
+    close(server_socket);
+    return EXIT_SUCCESS;
 }
