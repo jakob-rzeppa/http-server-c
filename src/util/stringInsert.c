dce6e08c85b8a72b07855a2c40828120a719a993
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-20 16:33:23 +0000

    created function to insert string into longer string

---
+++ src/util/stringInsert.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+/**
+ * Inserts a string into another, starting at a given index. The index will be updated to be at the character after the last insertion.
+ *
+ * @param target the string to be inserted to
+ * @param targetBufferSize the max size of the string (including \0)
+ * @param index the index to start inserting to
+ * @param source the string to be inserted
+ */
+int stringInsert(char *target, int targetBufferSize, int *index, const char *source)
+{
+    int sourceLength = strlen(source);
+    if (*index + sourceLength >= targetBufferSize - 1)
+        return -1;
+
+    for (int i = 0; i < sourceLength; i++)
+    {
+        target[*index + i] = source[i];
+    }
+
+    *index += sourceLength;
+    target[*index] = '\0';
+    return 0;
+}
