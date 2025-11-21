45100dea6eace0ef55b359d380b88b09ee234519
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-21 20:14:59 +0000

    created a notes_controller to send a basic success message

---
+++ src/notes_controller.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "/workspaces/http-server-c/src/notes_repository.c"
+#include "/workspaces/http-server-c/src/response_handler.c"
+
+int handle_get_notes_request(int client_socket)
+{
+
+    send_response(client_socket, 200, "{\"message\": \"success\"}");
+}
+
+int handle_create_note_request(int client_socket)
+{
+
+    send_response(client_socket, 200, "{\"message\": \"success\"}");
+}
+
+int handle_update_note_request(int client_socket)
+{
+
+    send_response(client_socket, 200, "{\"message\": \"success\"}");
+}
+
+int handle_delete_note_request(int client_socket)
+{
+
+    send_response(client_socket, 200, "{\"message\": \"success\"}");
+}
