07f05c513fe04b1c7832edab9433a9e24e7e6108
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-22 08:45:01 +0000

    created logger.c to handle logs

--- src/logger.c
+++ src/logger.c
@@ -1,5 +1,18 @@
 #include "/workspaces/http-server-c/src/common.h"
 
+void log_info(const char *format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+
+    fprintf(stderr, "[INFO] ");
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+
+    va_end(args);
+}
+
 void log_error(const char *format, ...)
 {
     va_list args;
