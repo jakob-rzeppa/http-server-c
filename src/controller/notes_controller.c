d69b27d2de4c056eb04650911624e65577c5f8e5
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-25 09:23:57 +0000

    implemented notes_repository update_note

--- src/controller/notes_controller.c
+++ src/controller/notes_controller.c
@@ -37,12 +37,21 @@ int handle_create_note_request(int client_socket, char *path, char *body)
 
 int handle_update_note_request(int client_socket, char *path, char *body)
 {
+    int id = extract_notes_id(path);
+    if (id == FAILED)
+    {
+        send_response(client_socket, 400, "{\"message\": \"invalid path: couldn't extract id\"}");
+        return FAILED_AND_SEND_RESPONSE;
+    }
+
     char *content = parse_update_request(body); // MAKE SURE TO FREE THE CONTENT
     if (content == NULL)
     {
         send_response(client_socket, 400, "{\"message\": \"invalid request body\"}");
         return FAILED_AND_SEND_RESPONSE;
     }
+
+    update_note(id, content);
 
     send_response(client_socket, 200, content);
 
--- src/repository/notes_repository.c
+++ src/repository/notes_repository.c
@@ -82,6 +82,109 @@ int create_note(char *content)
 
 int update_note(int id, char *content)
 {
+    // --- ASSEMBLE THE LINE ---
+    int content_size = strlen(content);
+    int line_size = 3 /* seperators */ + 10 /* id size */ + 10 /* content_size size */ + content_size + 1 /* esc char */;
+    char *line = (char *)malloc(sizeof(char) * (line_size));
+
+    int snprintf_res = snprintf(line, line_size, "%d;%d;%s\n", id, content_size, content);
+    if (snprintf_res < 0 || snprintf_res >= line_size)
+    {
+        log_error("create_note failed: content buffer overflow");
+        free(line);
+        return FAILED;
+    }
+
+    FILE *file;
+    FILE *temp;
+
+    bool update_has_happened = false;
+
+    // --- STORE A COPY OF THE FILE WITH THE UPDATED LINE ---
+    file = fopen(DATABASE_PATH, "r" /* read mode */);
+    if (file == NULL)
+    {
+        log_error("update_note failed: could not open file: %s", strerror(errno));
+        free(line);
+        return FAILED;
+    }
+
+    temp = fopen("update_notes.tmp", "w" /* write mode */);
+    if (temp == NULL)
+    {
+        log_error("update_note failed: could not open temp file: %s", strerror(errno));
+        free(line);
+        fclose(file);
+        return FAILED;
+    }
+
+    char *file_line_buffer = (char *)malloc(sizeof(char) * DATABASE_LINE_BUFFER_SIZE);
+
+    while (fgets(file_line_buffer, DATABASE_LINE_BUFFER_SIZE - 1, file) != NULL)
+    {
+        // atoi will stop reading at the first non-numeric character (';')
+        int current_id = atoi(file_line_buffer);
+
+        if (current_id == id)
+        {
+            fprintf(temp, line);
+            update_has_happened = true;
+        }
+        else
+        {
+            fprintf(temp, file_line_buffer);
+        }
+    }
+
+    free(file_line_buffer);
+    fclose(temp);
+    temp = NULL;
+    fclose(file);
+    file = NULL;
+
+    if (!update_has_happened)
+    {
+        log_error("update_note failed: id not found");
+        remove("update_notes.tmp");
+        free(line);
+        return FAILED;
+    }
+
+    // --- WRITE THE COPY TO THE FILE ---
+    temp = fopen("update_notes.tmp", "r" /* read mode */);
+    if (temp == NULL)
+    {
+        log_error("update_note failed: could not open temp file: %s", strerror(errno));
+        free(line);
+        return FAILED;
+    }
+
+    file = fopen(DATABASE_PATH, "w" /* write mode */);
+    if (file == NULL)
+    {
+        log_error("update_note failed: could not open file: %s", strerror(errno));
+        fclose(temp);
+        free(line);
+        return FAILED;
+    }
+
+    char *temp_line_buffer = (char *)malloc(sizeof(char) * DATABASE_LINE_BUFFER_SIZE);
+
+    while (fgets(temp_line_buffer, DATABASE_LINE_BUFFER_SIZE - 1, temp) != NULL)
+    {
+        fprintf(file, temp_line_buffer);
+    }
+
+    free(line);
+    free(temp_line_buffer);
+    fclose(file);
+    file = NULL;
+    fclose(temp);
+    temp = NULL;
+
+    // --- DELETE TEMP FILE ---
+    remove("update_notes.tmp");
+
     return SUCCESSFUL;
 }
 
@@ -131,7 +234,6 @@ int delete_note(int id)
     if (temp == NULL)
     {
         log_error("delete_note failed: could not open temp file: %s", strerror(errno));
-        fclose(file);
         return FAILED;
     }
 
@@ -139,6 +241,7 @@ int delete_note(int id)
     if (file == NULL)
     {
         log_error("delete_note failed: could not open file: %s", strerror(errno));
+        fclose(temp);
         return FAILED;
     }
 
